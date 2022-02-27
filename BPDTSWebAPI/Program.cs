9d2768d69a9f20d17b2c53d47044e1850128fa36
Anthony Osei-Agyemang <anthony.osei-agyemang@clinisafe.com>
2022-02-27 00:40:35 +0000

    Add Serilog for logging application errors in a text file

--- BPDTSWebAPI/Program.cs
+++ BPDTSWebAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,36 @@ namespace BPDTSWebAPI
     {
         public static void Main(string[] args)
         {
-            // Initialize Client
-            RestApiService.InitializeClient();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                path: "c:\\bpdtwebapi\\logs\\log-.txt",  // logfile with filename
+                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {UserId} {Event} - {Message}{NewLine}{Exception}", // logfile with timestamp
+                rollingInterval: RollingInterval.Day, // logfile with segments
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information // logfile with informaation
+            ).CreateLogger();
 
 
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                // Initialize Client
+                RestApiService.InitializeClient();
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Fatal(ex, "Application Failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() // logger for logging application errors
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
